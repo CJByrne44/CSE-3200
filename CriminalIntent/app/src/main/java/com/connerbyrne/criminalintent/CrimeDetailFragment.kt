6f4e085429897a6ae11dc87be0e0b963cd0838d4
CJByrne44 <70672489+CJByrne44@users.noreply.github.com>
2023-10-09 11:00:38 -0400

    I created crime

---
+++ CriminalIntent/app/src/main/java/com/connerbyrne/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,22 @@
+package com.connerbyrne.criminalintent
+
+import android.location.GpsSatellite
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CrimeDetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater : LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        
+    }
+}
