7a3c29663e99a5bf748f3c44c2a0a6aca7743858
CJByrne44 <70672489+CJByrne44@users.noreply.github.com>
2023-10-11 10:58:24 -0400

    class 10/11

--- CriminalIntent/app/src/main/java/com/connerbyrne/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/connerbyrne/criminalintent/CrimeDetailFragment.kt
@@ -5,11 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.connerbyrne.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
 
 class CrimeDetailFragment : Fragment() {
+    private lateinit var binding : FragmentCrimeDetailBinding
+    private lateinit var crime : Crime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "Bad Stuff Crime",
+            date = Date(),
+            isSolved = false
+        )
     }
 
     override fun onCreateView(
@@ -17,6 +30,29 @@ class CrimeDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply{
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+            
+//            crimeDate.setText(crime.title)
+//            crimeDate.setOnClickListener {view : View ->
+//                crimeDate.setText(crime.title)
+//            }
+            crimeDate.apply {
+                text = crime.date.toString()
+                crimeDate.isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _,isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+        }
     }
 }
