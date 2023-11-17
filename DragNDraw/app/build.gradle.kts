5642f90bc25f2381c6546a970a2f6724a38eb026
CJByrne44 <70672489+CJByrne44@users.noreply.github.com>
2023-11-17 10:29:17 -0500

    finished box app type behavior

--- DragNDraw/app/build.gradle.kts
+++ DragNDraw/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.connerbyrne.dragndraw"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.connerbyrne.dragndraw"
---
+++ DragNDraw/app/src/main/java/com/connerbyrne/dragndraw/Box.kt
@@ -0,0 +1,20 @@
+package com.connerbyrne.dragndraw
+
+import android.graphics.PointF
+import kotlin.math.min
+import kotlin.math.max
+
+data class Box(val start: PointF) {
+    var end: PointF = start
+    val left: Float
+        get() = min(start.x, end.x)
+
+    val right: Float
+        get() = max(start.x, end.x)
+
+    val top: Float
+        get() = min(start.y, end.y)
+
+    val bottom: Float
+        get() = max(start.y, end.y)
+}
